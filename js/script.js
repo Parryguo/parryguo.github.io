e54f4c4bd733c40ecd2880924a02480976cc2b24
Parry Guo <parry.guo@gmail.com>
2015-04-06 02:51:47 +0800

    update my design blog

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var apiURL = "https://api.dribbble.com/v1/users/14318/shots" + "?callback=?"
+
+
+
+
+$.getJSON( apiURL, {
+    per_page: 6,
+    access_token: "bb7b80ad6bbc148eedfe2992c60b7cb0d6b93d48270578c736dc5e7f3ef2d543"
+})
+    .done(function(json){
+
+        for (var i = 0; i < 6; i++) {
+            var imgsrc = json.data[i].images.normal;
+            var html_url = json.data[i].html_url
+            var title = json.data[i].title
+            var template = "<a href='" + html_url + "' target='_blank'><span>" + title + "</span><img style='width:300px;" +
+                "height:225px;' src='" + imgsrc + "'></a>"
+            $("#dribbble").append(template)
+
+
+
+            $("a").attr('target', '_blank');
+
+        };
+    })
